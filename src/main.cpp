4a316d8d2e4990c8259cca02bc6bbd717cce4362
Basil Amin <basilaminxyz@gmail.com>
2026-05-01 21:18:19 +0100

    env setup

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include <MPU6050.h>
+#include <Servo.h>
+
+MPU6050 imu;
+
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("Testing ..... Testing");
+}
+
+void loop(){
+
+}
